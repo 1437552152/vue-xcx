dc355b2aaebb8c2d1b583c2cd6e44d9efef6bcf2
yeyifu <1437552152@qq.com>
2019-02-28 18:36:02 +0800

    first commit

---
+++ src/components/card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p class="card">
+      {{text}},你好啊小叶
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    console.log(this.text);
+  },
+  props: ["text"]
+};
+</script>
+
+<style>
+.card {
+  padding: 10px;
+}
+</style>
